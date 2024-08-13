f92dd8d6f80facba66a561ca549ef47dff57cd1e
Ridwan Abdu Rojak <ridwan.abdurojak@gmail.com>
2024-08-13 16:14:27 +0700

    add config db from env

--- models/database.py
+++ models/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://postgres:admin@localhost:5432/my_db"
+from config.otp_config import Config
+
+CONFIG=Config()
+DATABASE_URL = f"postgresql+psycopg2://{CONFIG.db_user}:{CONFIG.db_pass}@{CONFIG.db_host}:{CONFIG.db_port}/{CONFIG.db_name}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
